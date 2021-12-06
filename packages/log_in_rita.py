bc72c97ee7bab7383ed9d8ea7feef0be9b364f7d
Rithoshi <ghilardi.rita@hotmail.it>
2021-12-06 12:08:40 +0100

    updated version

--- packages/log_in_rita.py
+++ packages/log_in_rita.py
@@ -1,31 +1,81 @@
-import pandas as pd
-import argparse
-import hashlib
+# FUNCTION LOG-IN
 
-def log_in(username, password):
+def log_in(email, password):
     
-    df = pd.read_csv("/Users/rita/Desktop/LAB/db_users.csv", delimiter =";")
-	
+    db_employees = pd.read_csv("/Users/rita/Desktop/LAB/db_employees.csv")
+    db_users = pd.read_csv("/Users/rita/Desktop/LAB/db_users.csv")
     
-    for name in df["username"]:
-        if name == username:
-            line = list(df["username"]).index(name)
-            digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-            if df["password"][line] == digest_password:
-                print("Access allowed")
-                break
+    suffix = email.split("@")[1]
+   
+    #employees
+    if suffix == "gold1.com":
+        
+        for address in db_employees["email"]:
+            if address == email:
+                line = list(db_employees["email"]).index(address)
+                digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+                if db_employees["password"][line] == digest_password:
+                    print("Access allowed")
+                    break
                 
-            else:
-                print("Please check password")
-                break
+                else:
+                    print("Please check password")
+                    break
                 
                 
-        elif name != username and list(df["username"]).index(name) == len(df["username"])-1:
-            print("No correspondence")
+            elif address != email and list(db_employees["email"]).index(address) == len(db_employees["email"])-1:
+                print("Please, log-in as a user")
             
                 
-        else: 
-            continue
+            else: 
+                continue
+        
+      
+    #users   
+    else: 
+      
+        for mail in db_users["email"]:
+            if mail == email:
+                line = list(db_users["email"]).index(mail)
+                digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+                if db_users["password"][line] == digest_password:
+                    print("Access allowed")
+                    break
+                
+                else:
+                    print("Please check password")
+                    break
+                
+                
+            elif mail != email and list(db_users["email"]).index(mail) == len(db_users["email"])-1:
+                print("No correspondence")
+            
+                
+            else: 
+                continue
             
             
-log_in("Marco Visentin", "MV123456")
+log_in("francesca.signorello@gold1.com", "Sushi<3")
+
+
+
+# FUNCTION ADD USER
+
+def add_user(email, password):
+    
+    db_users = pd.read_csv("/Users/rita/Desktop/LAB/db_users.csv")
+    
+    suffix = email.split("@")[1]
+   
+    
+    if suffix == "gold1.com":
+        print("Invalid email")
+        
+    else:
+        digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        new_df = pd.DataFrame({ "ID": [len(db_users["password"])+1], "email": [email], "password": [digest_password]})
+        new_df.to_csv('db_users.csv', mode = "a", index = False, header = False)
+        print("Registration was successful")
+
+
+add_user("pietro.belli@gold1.com", "HAPPY")
