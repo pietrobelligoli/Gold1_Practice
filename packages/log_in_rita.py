e8359ef9b7034a8c6c7ccf953126e920b05040eb
Rithoshi <ghilardi.rita@hotmail.it>
2021-12-07 12:05:59 +0100

    add function

--- packages/log_in_rita.py
+++ packages/log_in_rita.py
@@ -1,11 +1,13 @@
-# FUNCTION LOG-IN
 import pandas as pd
+import argparse
 import hashlib
+
+# FUNCTION LOG-IN
 
 def log_in(email, password):
     
-    db_employees = pd.read_csv("/Users/rita/Desktop/LAB/db_employees.csv")
-    db_users = pd.read_csv("/Users/rita/Desktop/LAB/db_users.csv")
+    db_employees = pd.read_csv(r'csv_file/db_employees.csv')
+    db_users = pd.read_csv(r'csv_file/db_users.csv')
     
     suffix = email.split("@")[1]
    
@@ -60,24 +62,81 @@ def log_in(email, password):
 log_in("francesca.signorello@gold1.com", "Sushi<3")
 
 
+# FUNCTION ADD_USER
 
-# FUNCTION ADD USER
+from email_validator import validate_email, EmailNotValidError
 
 def add_user(email, password):
     
-    db_users = pd.read_csv("/Users/rita/Desktop/LAB/db_users.csv")
+    db_users = pd.read_csv(r'csv_file/db_users.csv')
     
-    suffix = email.split("@")[1]
-   
-    
-    if suffix == "gold1.com":
-        print("Invalid email")
+    if "@" not in email:
+        print("Please enter a valid email")
         
-    else:
-        digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        new_df = pd.DataFrame({ "ID": [len(db_users["password"])+1], "email": [email], "password": [digest_password]})
-        new_df.to_csv('db_users.csv', mode = "a", index = False, header = False)
-        print("Registration was successful")
+    else: 
+        suffix = email.split("@")[1]
+   
+        if suffix == "gold1.com":
+            print("Invalid email, please register as a user")
+        
+        else:
+            try:
+                valid = validate_email(email)
+                email = valid.email
+                digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+                new_df = pd.DataFrame({ "ID": [len(db_users["password"])+1], "email": [email], "password": [digest_password]})
+                new_df.to_csv('db_users.csv', mode = "a", index = False, header = False)
+                print("Registration was successful!")
 
 
-add_user("pietro.belli@gold1.com", "HAPPY")
+            except EmailNotValidError as e:
+                print(str(e))
+
+        
+
+add_user("pietro.belligoli@gold1.com", "HAPPY")
+
+# FUNCTION ADD_EMPLOYEE
+
+def add_employee(email, password):
+    
+    df_employees = pd.read_csv(r'csv_file/employees.csv')
+    db_employees = pd.read_csv(r'csv_file/db_employees.csv')
+    
+    try: 
+        valid = validate_email(email)
+        email = valid.email
+        
+        if "@gold1.com" not in email:
+            print("Please enter an employee email")
+
+        else:
+            check = False
+            for mail in db_employees["email"]:
+                if mail == email:
+                    check = True
+                    print("This account is already registered")
+                    break
+
+            if check == False:     
+                presence = False
+                for mail in df_employees["Email"]:
+                    if email == mail:
+                        presence = True
+                        digest_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+                        new_df = pd.DataFrame({"email": [email], "password": [digest_password]})
+                        db_employees = db_employees.append(new_df)
+                        db_employees.to_csv(r'csv_file/employees.csv', index = False)
+                        print("Registration was successful!")
+                        break
+
+                if presence == False: 
+                    print("No correspondence")
+                    
+    except EmailNotValidError as e:
+        print(str(e))
+                        
+                       
+add_employee("marco.visentin@gmail.com", "HAPPY")
+
+
