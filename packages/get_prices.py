8fb2da09a428482dd3508c814a857df76f829444
Marco Visentin <marco.visentin@student.h-farm.com>
2021-12-14 15:40:15 +0100

    Change in get prices

--- packages/get_prices.py
+++ packages/get_prices.py
@@ -1,26 +1,32 @@
 def get_prices():
-  import requests
+    import requests
 
-  symbol = 'XAU,XAG,XPD,XPT,XRH'
-  base_currency = 'EUR'
-  endpoint = 'latest'
-  access_key = 'o65ww2g1zd18fqm7euy9h2at766a9amft1r6srflwfle1gqu6lx029fshbfu'
+    symbol = 'XAU,XAG,XPD,XPT,XRH'
+    base_currency = 'EUR'
+    endpoint = 'latest'
+    access_key = 'o65ww2g1zd18fqm7euy9h2at766a9amft1r6srflwfle1gqu6lx029fshbfu'
 
-  resp = requests.get('https://metals-api.com/api/'+endpoint+'?access_key='+access_key+'&base='+base_currency+'&symbols='+symbol)
-  s = resp.json()['success']
-  rates=resp.json()['rates']
+    resp = requests.get('https://metals-api.com/api/'+endpoint+'?access_key='+access_key+'&base='+base_currency+'&symbols='+symbol)
+    s = resp.json()['success']
+    
+    if s == False:
+        proces = None
+    
+    else:
+    
+        rates=resp.json()['rates']
 
-  oz=28.3495
-  
-  conversion={'XAU' : 'Gold',
-              'XAG' : 'Silver',
-              'XPD' : 'Palladium',
-              'XPT' : 'Platinum',
-              'XRH' : 'Rhodium'}
+        oz=28.3495
 
-  prices={}
-  
-  for key in rates.keys():
-    prices[conversion[key]]=round((rates[key]/oz),3)
-  
-  return(s,prices)
+        conversion={'XAU' : 'Gold',
+                  'XAG' : 'Silver',
+                  'XPD' : 'Palladium',
+                  'XPT' : 'Platinum',
+                  'XRH' : 'Rhodium'}
+
+        prices={}
+
+        for key in rates.keys():
+        prices[conversion[key]]=round((rates[key]/oz),3)
+
+    return(s,prices)
